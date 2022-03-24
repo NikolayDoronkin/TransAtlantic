b77dd419f262e4ae9be5cc891e6477c443ee34c5
Nikolay Doronkin Mejia <nicolaydm@gmail.com>
2022-03-25 01:09:26 +0300

    Добавлен swagger 4 версии

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfiguration = new DocumentBuilder()
+    .setTitle("trans-atlantic-api")
+    .setDescription("documentation for endpoints")
+    .setVersion('4.0.0')
+    .addTag('TA')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfiguration);
+  SwaggerModule.setup('/api/swagger-ui/index.html', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
